065f18669b52aa932221defabf52ef16fd8c10e3
Bruno <mazzabruno44@gmail.com>
2026-09-22 05:09:50 -0300

    Conectar Gestion de Usuarios a Firebase (nuevo proyecto)
    El panel de admin creaba usuarios solo en localStorage, sin quedar
    habilitados para loguearse de verdad. Ahora crea la cuenta en Firebase
    Auth y el perfil en Firestore, igual que setup-usuarios.html. Se
    actualizo firebase-config.js al nuevo proyecto (el anterior tenia una
    API key invalida) y se limpio codigo muerto en login.html.

--- Educar para tranformar/src/js/firebase-config.js
+++ Educar para tranformar/src/js/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfuHdOoAfoeU3npOJGwarV7QS3Iyb0BZE",
-  authDomain: "escuela-3ca50.firebaseapp.com",
-  projectId: "escuela-3ca50",
-  storageBucket: "escuela-3ca50.firebasestorage.app",
-  messagingSenderId: "537908816580",
-  appId: "1:537908816580:web:2780ea91f5c6d097ccb791"
+  apiKey: "AIzaSyBB3vR5-P27fjdQHXrF8NIpL2izh7kaFSQ",
+  authDomain: "escuela-61c37.firebaseapp.com",
+  projectId: "escuela-61c37",
+  storageBucket: "escuela-61c37.firebasestorage.app",
+  messagingSenderId: "953747301951",
+  appId: "1:953747301951:web:a6b4375c86963fdbbeba8a"
 };
 
 const app = initializeApp(firebaseConfig);
